f0fd251e2e8ad04cc1e9b58dcafb1b6b17c69067
Florent Jaouali <florent.jaouali@gmail.com>
2019-11-17 15:08:24 +0100

    Procfile

--- config/puma.rb
+++ config/puma.rb
@@ -8,6 +8,13 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
+# Heroku recommendations
+preload_app!
+on_worker_boot do
+  # Valid on Rails 4.1+ using the `config/database.yml` method of setting `pool` size
+  ActiveRecord::Base.establish_connection
+end
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port        ENV.fetch("PORT") { 3000 }
